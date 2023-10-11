adc10aee08d92c21f9072d2c6f43a55cb743b43f
Iowel <akiko3000akiko@gmail.com>
2023-10-11 18:50:27 +0300

    Create alpha app

---
+++ back/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from routers.user_router import user_router
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Тестовое приложение",
+        version="0.0.1a",
+    )
+
+    # добавление роутера функцией include_router
+    app.include_router(user_router)
+
+    return app
+
+app = create_app
